4b7b83b33475c2bd200635ede0b4b691efa647d5
DIPAKK2310 <dipakkhare217@gmail.com>
2025-08-11 00:08:26 +0530

    Added env file to project

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+import dotenv from 'dotenv';
+
+dotenv.config({
+    path:"./.env",
+});
+let myName = process.env.MY_NAME 
+
+
+console.log("Myname is:",myName)
 console.log("App is running...");
+
